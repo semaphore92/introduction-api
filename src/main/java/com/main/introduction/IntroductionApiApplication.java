abece54c8b11d492cd9248e4076d7b7b46f3cdaf
seokgu <swheo92@gmail.com>
2022-10-18 21:51:39 +0900

    Spring Boot Project 생성

---
+++ src/main/java/com/main/introduction/IntroductionApiApplication.java
@@ -0,0 +1,13 @@
+package com.main.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionApiApplication.class, args);
+	}
+
+}
