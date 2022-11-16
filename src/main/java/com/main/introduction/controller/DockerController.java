97bbfdc10bc189a2d1889ca7f483458b49397f4c
seokgu <swheo92@gmail.com>
2022-11-16 23:30:59 +0900

    Annotating class with @Schema(additionalProperties = AdditionalPropertiesValue.FALSE) does not produce JSON/YAML with additionalProperties: false #1927

--- src/main/java/com/main/introduction/controller/DockerController.java
+++ src/main/java/com/main/introduction/controller/DockerController.java
@@ -1,5 +1,9 @@
 package com.main.introduction.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class DockerController {
 
-    @GetMapping("/")
+    @GetMapping("/docker")
+    @Schema(additionalProperties = Schema.AdditionalPropertiesValue.FALSE)
+    @Operation(summary = "Returns List of Audit Log records based on the matching search critera", description = "Returns list of Audit Log records matching the search critera. SLA:500", responses = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of list of Audit Log records matching the search criteria", content = {
+                    @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "Invalid input provided", content = { @Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(mediaType = "application/json")}),})
     public String home(){
         return "docker Start";
     }
