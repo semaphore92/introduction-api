998a844aebc5fb5c251202fe181ee6647bee77cc
seokgu <swheo92@gmail.com>
2022-11-21 22:42:02 +0900

    build.gradle 테스트

--- src/main/java/com/main/introduction/controller/DockerController.java
+++ src/main/java/com/main/introduction/controller/DockerController.java
@@ -1,5 +1,6 @@
 package com.main.introduction.controller;
 
+import com.main.introduction.config.SecurityConfig;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Schema(additionalProperties = Schema.AdditionalPropertiesValue.FALSE)
 @RequestMapping("api")
 public class DockerController {
 
     @GetMapping("/docker")
-    @Schema(additionalProperties = Schema.AdditionalPropertiesValue.FALSE)
-    @Operation(summary = "Returns List of Audit Log records based on the matching search critera", description = "Returns list of Audit Log records matching the search critera. SLA:500", responses = {
+    @Operation(summary = "Returns List of Audit Log records based on the matching search critera", description = "Returns list of Audit Log records matching the search critera. SLA:500",
+            responses = {
             @ApiResponse(responseCode = "200", description = "Successful retrieval of list of Audit Log records matching the search criteria", content = {
-                    @Content(mediaType = "application/json") }),
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = SecurityConfig.class)) }),
             @ApiResponse(responseCode = "400", description = "Invalid input provided", content = { @Content(mediaType = "application/json")}),
             @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(mediaType = "application/json")}),})
     public String home(){
