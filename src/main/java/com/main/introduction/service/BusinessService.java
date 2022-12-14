a1dc1ae003adb75a74040100ccdc3a103fb93cda
seokgu <swheo92@gmail.com>
2022-12-14 23:08:26 +0900

    JPA TEST

--- src/main/java/com/main/introduction/service/BusinessService.java
+++ src/main/java/com/main/introduction/service/BusinessService.java
@@ -1,10 +1,24 @@
 package com.main.introduction.service;
 
+import com.main.introduction.repository.BusinessRepository;
+import com.main.introduction.vo.Person;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BusinessService {
 
+    @Autowired
+    private BusinessRepository businessRepository;
+
+    public List<Person> findAll(){
+        List<Person> personList = new ArrayList<>();
+        businessRepository.findPerson("hsw").forEach(e -> personList.add(e));
+        return personList;
+    }
 }
