2183d4771f98110ff6d047a899e6bfe10d8baecb
seokgu <swheo92@gmail.com>
2022-12-19 22:44:05 +0900

    12-19

--- src/main/java/com/main/introduction/vo/Person.java
+++ src/main/java/com/main/introduction/vo/Person.java
@@ -1,18 +1,26 @@
 package com.main.introduction.vo;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
 
 @Data
 @AllArgsConstructor // 모든 필드 값을 파라미터로 받은 생성자 생성
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 생성
 @Entity(name="person")
+@Getter
 public class Person {
+
     @Id
+    @GeneratedValue
     private String id;
+
+    @ManyToMany
+    @JoinTable(
+            name= "comment_keyword",
+            joinColumns = @JoinColumn(name="comment_id"),
+            inverseJoinColumns = @JoinColumn(name="keyword_id")
+    )
+    private Set<Person> persons = new HashSet<>();
 }
