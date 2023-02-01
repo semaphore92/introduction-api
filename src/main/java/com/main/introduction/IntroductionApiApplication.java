5fc6daef1d6f0bb1410595504bf901743048ac15
seokgu <swheo92@gmail.com>
2023-02-01 23:12:34 +0900

    [02-01]Eureka Server

--- src/main/java/com/main/introduction/IntroductionApiApplication.java
+++ src/main/java/com/main/introduction/IntroductionApiApplication.java
@@ -2,12 +2,11 @@ package com.main.introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.stereotype.Component;
 
 
+@EnableEurekaServer
 @EnableJpaAuditing // Auditing 기능 사용을 위해서는 Run Application Class에 해당 어노테이션 추가 
 @SpringBootApplication
 public class IntroductionApiApplication {
