ea0121dfe8948dfdccb3fcd633e2d8dab4d142e3
seokgu <swheo92@gmail.com>
2023-02-26 16:05:22 +0900

    [02-26] Zull API Gateway

--- src/main/java/com/main/introduction/IntroductionApiApplication.java
+++ src/main/java/com/main/introduction/IntroductionApiApplication.java
@@ -2,15 +2,10 @@ package com.main.introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.client.RestTemplate;
 
 
 @EnableEurekaServer
-@EnableJpaAuditing // Auditing 기능 사용을 위해서는 Run Application Class에 해당 어노테이션 추가 
 @SpringBootApplication
 public class IntroductionApiApplication {
 	public static void main(String[] args) {
--- src/main/java/com/main/introduction/config/SecurityConfig.java
+++ src/main/java/com/main/introduction/config/SecurityConfig.java
@@ -1,38 +1,12 @@
 package com.main.introduction.config;
 
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        /*
-            CSRF(Cross site Request forgery)
-              - 정상적인 사용자가 의도치 않은 위조요청을 보내는 행위
-
-            * Rest API에서 CSRF를 Disable 하는 이유
-              - Spring security Document quotation
-                non-browser client 서비스면 CSRF Disable 허용
-                rest api 서버라면 session 기반과 다르게 Stateless하기 때문에 서버에 인증 정보를 보관하지 않음.
-                rest api에서 client는 권한이 필요한 요청을 하기 위해서는 요청에 필요한 정보를(Oauth2, jwt토큰 등)을 포함시켜야한다.
-         */
-
-        // 인가 정책 설정
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/**")
-                .permitAll();
-    }
-
-    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("password")).roles("USER");
-    }
 }
